32cb27a298058cc7b93fdb2a5084e7cf28c274c8
Kevinlinpr <kevinlinpr@gmail.com>
2019-04-10 17:31:24 +0800

    first commit

---
+++ arm_control/src/joint_trajectory_controller.cpp
@@ -0,0 +1,15 @@
+//
+// Created by kevinlinpr on 19-4-3.
+//
+
+#include <arm_control/effort_joint_interface.h>
+#include <pluginlib/class_list_macros.h>
+
+namespace arm_control
+{
+    typedef joint_trajectory_controller::JointTrajectoryController<trajectory_interface::QuinticSplineSegment<double>,
+    hardware_interface::EffortJointInterface> JointTrajectoryController;
+
+} // namespace
+
+PLUGINLIB_EXPORT_CLASS(arm_control::JointTrajectoryController, controller_interface::ControllerBase);
